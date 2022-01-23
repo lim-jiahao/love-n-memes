6381dab46a2d4a8e836ca4f4e35db52e86d09c2d
daves <davidlimjh77@gmail.com>
2022-01-23 22:02:38 +0800

    matching system working

--- controllers/SwipeController.mjs
+++ controllers/SwipeController.mjs
@@ -7,23 +7,50 @@ export default class SwipeController extends BaseController {
 
   async createSwipe(req, res) {
     if (!req.userId) {
-      res.status(403).send({ message: 'Login to swipe sir' });
+      res.status(403).send({ message: 'Login to swipe sir' }).end();
+      return;
     }
-    console.log(req.body);
+    console.log(req.userId, 'userId');
+    console.log(req.body, 'requestBody');
     const { swipeeId } = req.body;
     if (!(swipeeId === req.userId)) {
-      const [swipe, created] = await this.model.findOrCreate({
+      // actually have to find if swipe alr exist in either swiper swipee
+      // relationship alr exists then create match
+      const haveBeenSwipedBefore = await this.model.findOne({
         where: {
-          swiperId: req.userId,
-          swipeeId,
-        },
-        defaults: {
-          swipedRight: false,
+          swiperId: swipeeId,
+          swipeeId: req.userId,
         },
       });
-      console.log(swipe);
-      if (created) res.status(200).send({ message: 'successfully created swipe' });
-      else res.status(201).send({ message: "you've already swiped this user" });
-    } else res.status(201).send({ message: 'cant swipe urself man' });
+      console.log(haveBeenSwipedBefore);
+      if (haveBeenSwipedBefore) {
+        // user has already been swiped by the swipee
+        // update model with right swiped and
+        haveBeenSwipedBefore.set({ swipedRight: true });
+        await haveBeenSwipedBefore.save();
+
+        // create match model
+        await this.db.Match.create({
+          matcherId: req.userId,
+          matcheeId: swipeeId,
+        });
+        res.status(201).send({ message: 'a match has been made!' });
+      } else {
+        // user has not been swiped on by swipee, so create new
+        // swipe model
+        const [swipe, created] = await this.model.findOrCreate({
+          where: {
+            swiperId: req.userId,
+            swipeeId,
+          },
+          defaults: {
+            swipedRight: false,
+          },
+        });
+        console.log(swipe);
+        if (created) res.status(201).send({ message: 'successfully created swipe' });
+        else res.status(400).send({ message: "you've already swiped this user" });
+      }
+    } else res.status(400).send({ message: 'cant swipe urself man' });
   }
 }
