4fe5286e4561d1d596339fe0a1f095c0a3924493
lim-jiahao <lim.jiahaoo@gmail.com>
2022-02-03 00:01:27 +0800

    unswiped users query - age and location clauses need to be moved up, idk weird

--- controllers/UserController.mjs
+++ controllers/UserController.mjs
@@ -146,6 +146,22 @@ export default class UserController extends BaseController {
           ],
         },
 
+        // ensure age within user's filter (two way)
+        age: {
+          [Op.and]: [
+            { [Op.gte]: user.ageMin },
+            { [Op.lte]: user.ageMax },
+          ],
+        },
+
+        [Op.and]: [
+          { ageMin: { [Op.lte]: user.age } },
+          { ageMax: { [Op.gte]: user.age } },
+        ],
+
+        // use previously defined location clause to filter on location preferences
+        ...locationClause,
+
         // ensure mutual interest between the users
         [Op.and]: [
           {
@@ -164,23 +180,6 @@ export default class UserController extends BaseController {
         '$purposes.id$': {
           [Op.in]: purposeArray,
         },
-
-        // ensure age within user's filter (two way)
-        age: {
-          [Op.and]: [
-            { [Op.gte]: user.ageMin },
-            { [Op.lte]: user.ageMax },
-          ],
-        },
-
-        [Op.and]: [
-          { ageMin: { [Op.lte]: user.age } },
-          { ageMax: { [Op.gte]: user.age } },
-        ],
-
-        // use previously defined location clause to filter on location preferences
-        ...locationClause,
-
       },
       include: [
         {
